46711e68a6a841106757fcdff71d924b81d6022e
Danny Ruiz <ynnadrules@gmail.com>
2014-07-09 16:20:41 -0500

    breaking version

--- app/master_layout.rb
+++ app/master_layout.rb
@@ -10,22 +10,22 @@ class MasterLayout < MK::Layout
 
     constraints :tableView do
       width.is(:superview)
-      height.is(:superview)
-      # bottom.is(:superview)
+      # height.is(:superview)
+      bottom.is(:superview)
     end
 
-    # constraints :special_header do
-    #   width.is(:superview)
-    # end
+    constraints :special_header do
+      width.is(:superview)
+    end
   end
 
   def layout
-    # add SpecialHeaderLayout.new.view, :special_header do
-    #   constraints do
-    #     origin [0, 0]
-    #     height 54
-    #   end
-    # end
+    add SpecialHeaderLayout.new.view, :special_header do
+      constraints do
+        origin [0, 0]
+        height 54
+      end
+    end
 
     add UITableView, :tableView do
       background_color UIColor.whiteColor
@@ -33,8 +33,8 @@ class MasterLayout < MK::Layout
       constraints do
         x 0
 
-        y 0
-        # below(:special_header)
+        # y 0
+        below(:special_header)
       end
     end
   end #end layout
